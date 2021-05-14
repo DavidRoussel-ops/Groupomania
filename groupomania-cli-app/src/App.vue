69aa247d10d034d25db058a03bf80d4ec91839ee
David <davidroussel171113@outlook.fr>
2021-05-14 21:16:40 +0200

    Instalation et mise en place app Vue.cli

---
+++ groupomania-cli-app/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <nav>
+      <Homepage url="#" text="Accueil"/>
+      <Homepage url="#" text="A propos"/>
+      <Homepage url="#" text="Contact"/>
+    </nav>
+    <p>Bienvenue sur la page de <Homepage text="Groupomania"/>!</p>
+  </div>
+</template>
+
+<script>
+import Homepage from "@/components/sign";
+
+export default {
+  name: 'App',
+  components: {
+    Homepage,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
