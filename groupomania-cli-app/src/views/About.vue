28aaff1bd37556ec6b69ba8fc9ccaf2954292988
David <davidroussel171113@outlook.fr>
2021-08-23 15:06:07 +0200

    Mise en place du style page de connexion/inscription.

--- groupomania-cli-app/src/views/About.vue
+++ groupomania-cli-app/src/views/About.vue
@@ -1,12 +1,47 @@
 <template>
   <div>
     <img alt="Groupomania logo" src="../assets/icon.png">
+    <label id="id" for="mail">Email</label><input id="mail" type="email">
+    <label id="mdp" for="pass">Mots de passe</label><input id="pass" type="password">
   </div>
 </template>
 
 <style>
+body{
+  background-repeat: no-repeat;
+  background-position: center;
+  background-position-y: center;
+}
+
 img{
-  display: inline;
   position: relative;
+}
+
+#mail{
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 40%;
+}
+
+#pass {
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 50%;
+}
+
+#id {
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 35%;
+}
+
+#mdp {
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 45%;
 }
 </style>
--- groupomania-cli-app/src/views/Home.vue
+++ groupomania-cli-app/src/views/Home.vue
@@ -1,14 +1,62 @@
 <template>
   <div class="home">
-    <img alt="Groupomania logo" src="../assets/icon.png">
+    <img id="fond" alt="Groupomania logo" src="../assets/icon.png">
+    <label id="id" for="mail">Email</label><input id="mail" type="email">
+    <label id="mdp" for="pass">Mots de passe</label><input id="pass" type="password">
   </div>
 </template>
+
+<script>
+  /*export default {
+    data() {
+      return alert('salut')
+    },
+    beforeCreate() {console.log('Je ne suis pas encore crée')},
+    created() {console.log('Je suis crée')},
+    beforeMount() {console.log('Je vais bientot être sur le dom')},
+    mounted() {console.log('Je suis sur le DOM')},
+    beforeUnmount() {console.log('Je vais disparaitre')},
+    unmounted() {console.log('Je suis supprimé')}
+  }*/
+</script>
 
 <style>
 body{
   background-repeat: no-repeat;
   background-position: center;
   background-position-y: center;
+}
+
+img{
+  position: relative;
+}
+
+#mail{
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 40%;
+}
+
+#pass {
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 50%;
+}
+
+#id {
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 35%;
+}
+
+#mdp {
+  width: 20%;
+  position: absolute;
+  left: 40%;
+  top: 45%;
 }
 </style>
 
