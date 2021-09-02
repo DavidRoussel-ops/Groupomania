17bfeb14b276ebe0522279245fd7f85be5244a0d
David <davidroussel171113@outlook.fr>
2021-09-02 21:38:05 +0200

    ajout d'un script data et modification css

--- groupomania-cli-app/src/views/Signpage.vue
+++ groupomania-cli-app/src/views/Signpage.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="conteneur">
+    <div class="conteneur-page">
   <div class="contenue">
     <img alt="log-left" class="font-left" src="../assets/icon-left-font-monochrome-black.png">
     <div class="friends">
@@ -9,16 +10,71 @@
       <h2>Groupes</h2>
     </div>
   </div>
-  <div class="home">
-    <img alt="Groupomania logo" src="../assets/icon.png">
-  </div>
-    <i class="fas fa-edit"></i>
+    <div class="conteneur-comment">
+      <div class="comment" v-for="(comment, idx) in comments" v-bind:key="idx">
+        <h3 class="nom">{{ comment.name }}</h3>
+        <p>{{ comment.commentaire }}</p>
+        <span>{{ comment.note }}</span>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data: () => {
+    return {
+      comments: [{
+        name: 'Michel',
+        commentaire: 'Cette appli c\'est de la bombe !!',
+        note: 5,
+      }, {
+        name: 'Nadine',
+        commentaire: 'Je trouve pas ça top moi',
+        note: 1,
+      }, {
+        name: 'Daniel',
+        commentaire: 'C\'est pas encore le top du top mas c\'est un bon début !',
+        note: 3,
+      }]
+    }
+  }
+}
+</script>
+
 <style>
 #conteneur{
+  height: 200px;
+  position: absolute;
+  width: 100%;
+}
+
+.conteneur-page{
+  width: 100%;
   display: flex;
+  z-index: 2;
+}
+
+.conteneur-comment{
+  background: url("../assets/icon.png") no-repeat center;
+  width: 80%;
+  z-index: 1;
+}
+
+.comment{
+  border-radius: 50px;
+  box-shadow: 4px 2px 2px lightslategray;
+  background-color: rgba(255,255,255,1);
+  opacity: 1;
+  width: 80%;
+  height: 200px;
+  margin: 50px auto 50px;
+  z-index: 2;
+}
+
+.nom{
+  padding-top: 20px;
 }
 
 .contenue{
@@ -37,10 +93,6 @@
 h2{
   box-shadow: 0 2px 2px lightslategray;
   padding: 10px;
-}
-
-.home{
-  width: 80%;
 }
 
 .font-left{
