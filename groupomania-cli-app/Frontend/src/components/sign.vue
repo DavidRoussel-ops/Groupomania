2b530170761849b799541376250f78088c7f9657
David <davidroussel171113@outlook.fr>
2021-09-19 17:23:05 +0200

    Ajout du dossier Frontend

---
+++ groupomania-cli-app/Frontend/src/components/sign.vue
@@ -0,0 +1,17 @@
+<template>
+  <a :href="url">{{ text }}</a>
+</template>
+
+<script>
+export default {
+  name : 'Homepage',
+  props : ['url', 'text']
+}
+</script>
+
+<style>
+  a {
+    text-decoration: none;
+    margin-right: 10px;
+  }
+</style>
---
+++ groupomania-cli-app/Frontend/src/views/About.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <img alt="Groupomania logo" src="../assets/icon.png">
+    <label id="id" for="mail">Email</label><input id="mail" type="email">
+    <label id="mdp" for="pass">Mots de passe</label><input id="pass" type="password">
+    <label id="fonction" for="poste">Poste</label><input id="poste" type="text">
+    <label id="surname" for="nom">Nom</label><input id="nom" type="text">
+    <label id="name" for="prenom">Prénom</label><input id="prenom" type="text">
+    <label id="old" for="age">Age</label><input id="age" type="number">
+    <button id="btninc" type="submit">Inscription</button>
+  </div>
+</template>
+
+<style>
+body{
+  background-repeat: no-repeat;
+  background-position: center;
+  background-position-y: center;
+}
+
+img{
+  position: relative;
+}
+
+#mail{
+  width: 20%;
+  position: absolute;
+  left: 20%;
+  top: 40%;
+}
+
+#pass {
+  width: 20%;
+  position: absolute;
+  left: 20%;
+  top: 50%;
+}
+
+#id {
+  width: 20%;
+  position: absolute;
+  left: 20%;
+  top: 35%;
+}
+
+#mdp {
+  width: 20%;
+  position: absolute;
+  left: 20%;
+  top: 45%;
+}
+
+#fonction {
+  width: 20%;
+  position: absolute;
+  left: 20%;
+  top: 55%;
+}
+
+#poste {
+  width: 20%;
+  position: absolute;
+  left: 20%;
+  top: 60%;
+}
+
+#surname{
+  width: 20%;
+  position: absolute;
+  left: 60%;
+  top: 35%;
+}
+
+#nom{
+  width: 20%;
+  position: absolute;
+  left: 60%;
+  top: 40%;
+}
+
+#name{
+  width: 20%;
+  position: absolute;
+  left: 60%;
+  top: 45%;
+}
+
+#prenom{
+  width: 20%;
+  position: absolute;
+  left: 60%;
+  top: 50%;
+}
+
+#old{
+  width: 20%;
+  position: absolute;
+  left: 60%;
+  top: 55%;
+}
+
+#age{
+  width: 20%;
+  position: absolute;
+  left: 60%;
+  top: 60%;
+}
+
+#btninc{
+  width: 10%;
+  position: absolute;
+  left: 45%;
+  top: 65%;
+}
+</style>
