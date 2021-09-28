510fb81cd23e14329a3d066c8f9cd06b14321993
David <davidroussel171113@outlook.fr>
2021-09-28 11:31:56 +0200

    Création de l'application

--- groupomania-cli-app/Backend/mysqlexperiment/app.js
+++ groupomania-cli-app/Backend/mysqlexperiment/app.js
@@ -1,23 +1,42 @@
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'stephanie',
-    password : 'Stephgroupo',
-    database : 'groupomania'
-});
+const express = require('express');
+const bodyParser = require('body-parser');
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connecter à MySQL Server!');
-    /*connection.query("CREATE DATABASE Groupomania", function (err, result){
-        if (err) throw err;
-        console.log("BDD crée!");
-    })*/
-});
+const app = express();
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields){
-    if (error) throw error;
-    console.log('La solution est : ', results[0].solution);
-});
+app.use(((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+}));
 
-connection.end();
+app.use(bodyParser.json());
+
+app.post('/api/stuff', ((req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({message : 'Post confirmé.'})
+}));
+
+app.use('/api/stuff', ((req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
+}))
+
+module.exports = app;
---
+++ groupomania-cli-app/Backend/mysqlexperiment/mysql.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'stephanie',
+    password : 'Stephgroupo',
+    database : 'groupomania'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connecter à MySQL Server!');
+    /*connection.query("CREATE DATABASE Groupomania", function (err, result){
+        if (err) throw err;
+        console.log("BDD crée!");
+    })*/
+});
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields){
+    if (error) throw error;
+    console.log('La solution est : ', results[0].solution);
+});
+
+//connection.end();
