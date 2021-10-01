8ebbce95017e0a794193466b134d64d992982273
David <davidroussel171113@outlook.fr>
2021-10-01 17:16:11 +0200

    Schema utilisateur

---
+++ groupomania-cli-app/Backend/mysqlexperiment/models/user.js
@@ -0,0 +1,9 @@
+const userSchema = {
+    id : {type : String, required : true},
+    email : {type : String, required : true, unique : true},
+    mots_de_passe : {type : String, required : true},
+    nom : {type : String, required : true},
+    prenom : {type : String, required : true},
+};
+
+module.exports = userSchema;
