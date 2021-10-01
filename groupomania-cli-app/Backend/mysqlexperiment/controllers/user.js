09181737434fd6f6ddd71ffd0a983ff393c9e21d
David <davidroussel171113@outlook.fr>
2021-10-01 17:10:46 +0200

    Mise en place des fonction ajout/modifier/supprimer utilisateur

---
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/user.js
@@ -0,0 +1,37 @@
+const User = require('../models/user')
+
+exports.createUser = (req, res, next) => {
+    const userInsert = req.query;
+    const insertUser = "INSERT INTO users SET ?";
+    const insert = new User({
+        ...userInsert
+    })
+    connection.query(insertUser,insert,(err,result,fields) => {
+        if (err) throw err;
+        res.json({saved : result.affectedRows})
+    });
+};
+
+exports.updateUser = (req, res, next) => {
+    const userUpdate = req.query;
+    const updateUser = "UPDATE users SET ?";
+    const update = User({
+        ...userUpdate
+    })
+    connection.query(updateUser,update,(err,result,fields) => {
+        if (err) throw err;
+        res.json({update : result.affectedRows})
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+    const userDelete = req.query;
+    const deleteUser = "DELETE FROM users SET ?";
+    const suppression = User({
+        ...userDelete
+    })
+    connection.query(deleteUser,suppression,(err,result,fields) => {
+        if (err) throw err;
+        res.json({delete : result.affectedRows})
+    });
+};
