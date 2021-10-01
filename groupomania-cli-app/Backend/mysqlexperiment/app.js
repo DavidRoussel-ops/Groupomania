443f8bdc048a1eb7c1fa9f606393351845033ceb
David <davidroussel171113@outlook.fr>
2021-10-01 17:20:35 +0200

    Ajout des constantes routes

--- groupomania-cli-app/Backend/mysqlexperiment/app.js
+++ groupomania-cli-app/Backend/mysqlexperiment/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//const mysqlServe = require('mysql');
+
+const postRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -25,56 +27,5 @@ app.use(((req, res, next) => {
 }));
 
 app.use(bodyParser.json());
-
-app.use("/save_user", (((req, res) => {
-    const newUser = req.query;
-    const query = "INSERT INTO users SET ?";
-    let TIMESTAMP = mysql.raw('now()');
-    const data = {
-        id : 1,
-        email : "martinjean@gmail.com",
-        mots_de_passe : "Supermartin",
-        nom : "Martin",
-        prenom : "Jean",
-    }
-    connection.query(query,data,(err,result,fields) => {
-        if (err) throw err;
-        res.json({saved : result.affectedRows})
-    });
-})));
-
-app.use("/save_post", ((req, res) => {
-    const request = req.query;
-    const query = "INSERT INTO post SET ?";
-    let CURRENT_TIMESTAMP = mysql.raw('now()');
-    const params = {
-        id : 1,
-        users : 1,
-        nom : "Martin",
-        prenom : "Jean",
-        date_diff : CURRENT_TIMESTAMP,
-        txt : "C'est génial !",
-        img : "",
-    }
-    connection.query(query,params,(err,result,fields) => {
-        if (err) throw err;
-        res.json({saved : result.affectedRows})
-    });
-}));
-
-app.use('/api/stuff', ((req, res, next) => {
-    const stuff = [
-        {
-            id : 1,
-            users : 1,
-            nom : 'Martin',
-            prenom : 'Jean',
-            date_diff : '29/09/2021 17h00',
-            txt : 'Hello World !',
-            img : ''
-        },
-    ];
-    res.status(200).json(stuff);
-}))
 
 module.exports = app;
