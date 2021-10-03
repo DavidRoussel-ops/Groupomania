415e9fa34d678401396ba61a44fc51d6fd843707
David <davidroussel171113@outlook.fr>
2021-10-03 22:05:02 +0200

    Ajout de la valeur unique à l'id

--- groupomania-cli-app/Backend/mysqlexperiment/models/user.js
+++ groupomania-cli-app/Backend/mysqlexperiment/models/user.js
@@ -1,5 +1,5 @@
 const userSchema = {
-    id : {type : String, required : true},
+    id : {type : String, required : true, unique : true},
     email : {type : String, required : true, unique : true},
     mots_de_passe : {type : String, required : true},
     nom : {type : String, required : true},
