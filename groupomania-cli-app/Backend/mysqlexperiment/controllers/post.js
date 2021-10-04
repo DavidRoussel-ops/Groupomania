39823330236ea7b9c31ab2d6ec71b25fc47d9fa8
David <davidroussel171113@outlook.fr>
2021-10-04 23:45:04 +0200

    Refonte du controllers post

--- groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
@@ -1,38 +1,65 @@
-const connection = require('../Security/mysql');
-const Post = require('../models/post');
+const {getPost, getPostById, insertPost, updatePostById, deletePostById} = require("../models/post");
 
-exports.createPost = (req, res, next) => {
-    const postInsert = req.query;
-    const insertPost = "INSERT INTO post SET ?";
-    const postIn = new Post({
-        ...postInsert
-    });
-    connection.query(insertPost,postIn,(err,result,fields) => {
-        if (err) throw err;
-        res.json({saved : result.affectedRows})
-    });
-};
-
-exports.updatePost = (req, res, next) => {
-    const postUpdate = req.query;
-    const updatePost = "UPDATE post SET txt=postUpdate, img=postUpdate WHERE id=postUpdate";
-    const postUp = Post({
-        ...postUpdate
-    })
-    connection.query(updatePost,postUp,(err,result,fields) => {
-        if (err) throw err;
-        res.json({update : result.affectedRows})
+exports.showPost = (req, res) => {
+    getPost((err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({Post : results})
+        }
     });
 };
 
-exports.deletePost = (req, res, next) => {
-    const postDelete = req.query;
-    const deletePost = "DELETE FROM post WHERE id=postDelete";
-    const postDel = Post({
-        ...postDelete
-    })
-    connection.query(deletePost,postDel,(err,result,fields) => {
-        if (err) throw err;
-        res.json({delete : result.affectedRows})
+exports.showPostById = (req, res) => {
+    getPostById(req.params.id, (err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({PostId : results})
+        }
+    });
+};
+
+exports.createPost = (req, res) => {
+    const data = req.body;
+    insertPost(data, (err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({postCreate : results})
+        }
+    });
+};
+
+exports.updatePost = (req, res) => {
+    const data = req.body;
+    const id = req.params.id;
+    updatePostById(data, id,(err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({postUpdate : results})
+        }
+    });
+};
+
+exports.deletePost = (req, res) => {
+    const id = req.params.id;
+    deletePostById(id, (err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({postdelete : results})
+        }
     });
 };
