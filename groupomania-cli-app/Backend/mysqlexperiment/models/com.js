e226bbfbd1c7f98142d10a9321dbf7b2b2a8314f
David <davidroussel171113@outlook.fr>
2021-10-04 23:46:46 +0200

    Refonte model commentaire

--- groupomania-cli-app/Backend/mysqlexperiment/models/com.js
+++ groupomania-cli-app/Backend/mysqlexperiment/models/com.js
@@ -1,7 +1,61 @@
-const comSchema = {
-    id : {type : String, required : true, unique : true},
-    users : {type : String, required : true},
-    com : {type : String, required : true}
+const connection = require('../Security/mysql');
+
+exports.getCom = (result) => {
+    connection.query('SELECT * FROM comment', (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            console.log({comment : results});
+            result(null, results);
+        }
+    });
 };
 
-module.exports = comSchema;
+exports.getComById = (id,result) => {
+    connection.query('SELECT * FROM comment WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            console.log({commentId : results});
+            result(null, results[0]);
+        }
+    });
+};
+
+exports.insertCom = (data, result) => {
+    connection.query('INSERT INTO comment SET ?', [data], (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            console.log({commentInsert : results});
+            result(null, results);
+        }
+    });
+};
+
+exports.updateComById = (data, id, result) => {
+    connection.query('UPDATE comment SET com = ? WHERE id = ?', [data.com, id], (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            console.log({commentUpdate : results});
+            result(null, results);
+        }
+    })
+};
+
+exports.deleteComByID = (id, result) => {
+    connection.query('DELETE FROM comment WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            console.log(err);
+            result(err, null);
+        } else {
+            console.log({commentDelete : results});
+            result(null, results);
+        }
+    })
+};
