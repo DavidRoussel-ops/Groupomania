e143851c484a06d280ebf2af07b6b71d02748fa0
David <davidroussel171113@outlook.fr>
2021-10-04 14:54:40 +0200

    Ajout d'une constante contenant les modules de securité

--- groupomania-cli-app/Backend/mysqlexperiment/app.js
+++ groupomania-cli-app/Backend/mysqlexperiment/app.js
@@ -1,15 +1,7 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
 const mysql = require('./Security/mysql');
+const moduleSecurity = require('./Security/module');
 const app = express();
-app.use(bodyParser.json());
-
-app.use(((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-}));
 
 const postRoutes = require('./routes/post');
 const userRoutes = require('./routes/user');
