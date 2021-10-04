0700a6924af909bf2870e8deac8066b10e5710e8
David <davidroussel171113@outlook.fr>
2021-10-04 23:49:38 +0200

    Modification et ajout route post

--- groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
-const postCtrl = require('../controllers/post');
+const {showPost, showPostById, createPost, updatePost, deletePost} = require('../controllers/post');
 
-router.use(bodyParser.json());
-
-router.post('/save_post', postCtrl.createPost);
-router.put('/update_post', postCtrl.updatePost);
-router.delete('/delete_post', postCtrl.deletePost);
+router.get('/post', showPost);
+router.get('/post/:id', showPostById);
+router.post('/post', createPost);
+router.put('/post/:id', updatePost);
+router.delete('/post/:id', deletePost);
 
 module.exports = router;
