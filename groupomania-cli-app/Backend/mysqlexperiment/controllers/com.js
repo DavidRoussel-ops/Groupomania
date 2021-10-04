964376a7ab883885ddd3c6b6bec269227bfe8237
David <davidroussel171113@outlook.fr>
2021-10-04 23:44:39 +0200

    Refonte du controllers commentaire

--- groupomania-cli-app/Backend/mysqlexperiment/controllers/com.js
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/com.js
@@ -1,38 +1,65 @@
-const connection = require('../Security/mysql');
-const Com = require('../models/com');
+const {getCom, getComById, insertCom, updateComById, deleteComByID} = require("../models/com");
 
-exports.createCom = (req, res, next) => {
-    const comInsert = req.query;
-    const insertCom = "INSERT INTO comment SET ?";
-    const comIn = new Com({
-        ...comInsert
-    });
-    connection.query(insertCom,comIn,(err,result,fields) => {
-        if (err) throw err;
-        res.json({saved : result.affectedRows})
-    });
-};
-
-exports.updateCom = (req, res, next) => {
-    const comUpdate = req.query;
-    const updateCom = "UPDATE comment SET com=comUpdate WHERE id=comUpdate";
-    const comUp = Com({
-        ...comUpdate
-    })
-    connection.query(updateCom,comUp,(err,result,fields) => {
-        if (err) throw err;
-        res.json({update : result.affectedRows})
+exports.showCom = (req, res) => {
+    getCom((err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({com : results})
+        }
     });
 };
 
-exports.deleteCom = (req, res, next) => {
-    const comDelete = req.query;
-    const deleteCom = "DELETE FROM comment  WHERE id=comDelete";
-    const comDel = Com({
-        ...comDelete
-    })
-    connection.query(deleteCom,comDel,(err,result,fields) => {
-        if (err) throw err;
-        res.json({delete : result.affectedRows})
+exports.showComById = (req, res) => {
+    getComById(req.params.id, (err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({comId : results})
+        }
+    });
+};
+
+exports.createCom = (req, res) => {
+    const data = req.body;
+    insertCom(data, (err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({comCreate : results})
+        }
+    });
+};
+
+exports.updateCom = (req, res) => {
+    const data = req.body;
+    const id = req.params.id;
+    updateComById(data, id,(err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({comUpdate : results})
+        }
+    });
+};
+
+exports.deleteCom = (req, res) => {
+    const id = req.params.id;
+    deleteComByID(id, (err, results) => {
+        if (err) {
+            res.send(err);
+            console.log(err)
+        } else {
+            res.json(results);
+            console.log({comdelete : results})
+        }
     });
 };
