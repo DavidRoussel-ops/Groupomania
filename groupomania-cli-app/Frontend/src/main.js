e2705f61a6791ab512e779d972ea5d5e8bd3ee56
David <davidroussel171113@outlook.fr>
2021-10-05 23:53:49 +0200

    ajout du store

--- groupomania-cli-app/Frontend/src/main.js
+++ groupomania-cli-app/Frontend/src/main.js
@@ -1,5 +1,40 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
+
+/*import { createApp } from 'vue'
+import { createRouter , createWebHashHistory } from 'vue-router'
+import App from './App.vue'
+import Create from './components/AddUser'
+import Edit from './components/EditUser'
+import Index from './components/UserList'
+
+createApp.use(createRouter)
+
+const routes = [
+    {
+        name: 'Create',
+        path: '/create',
+        component: Create
+    },
+    {
+        name: 'Edit',
+        path: '/edit/:id',
+        component: Edit
+    },
+    {
+        name: 'Index',
+        path: '/',
+        component: Index
+    },
+];
+
+const router = new createRouter({ mode : createWebHashHistory, routes : routes })
+
+new createApp({
+    router,
+    render : h => h(App),
+}).mount('#app');*/
