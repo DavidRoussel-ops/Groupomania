d623f0ff2e6ef153dd5663a9b8a2dbcb069ebbd2
David <davidroussel171113@outlook.fr>
2021-10-05 23:55:02 +0200

    Refonte du router

--- groupomania-cli-app/Frontend/src/router/index.js
+++ groupomania-cli-app/Frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter , createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home';
 
 const routes = [
   {
@@ -7,19 +7,6 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  {
-    path: '/inscription',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/Groupomania',
-    name: 'Signpage',
-    component: () => import('../views/Signpage.vue')
-  }
 ]
 
 const router = createRouter({
@@ -28,3 +15,6 @@ const router = createRouter({
 })
 
 export default router
+
+
+
