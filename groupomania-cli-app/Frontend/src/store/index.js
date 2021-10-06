69cafaecb7d1680beef7d309854f4ecf78269f49
David <davidroussel171113@outlook.fr>
2021-10-06 22:37:44 +0200

    Utilisation de axios et mise en place actions login/création compte

--- groupomania-cli-app/Frontend/src/store/index.js
+++ groupomania-cli-app/Frontend/src/store/index.js
@@ -1,5 +1,45 @@
 import { createStore } from "vuex"
 
-const store = createStore;
+const axios = require('axios');
+
+const instance = axios.create({
+    baseURL : 'http://localhost:3000/groupomania'
+});
+
+const instanceLog = axios.create({
+    baseURL : 'http://localhost:3000/groupomania/user'
+})
+
+const store = createStore({
+    state : {
+
+    },
+    actions : {
+        login : ({commit}, userInfos) => {
+            return new Promise((resolve, reject) => {
+                commit;
+                instanceLog.get('/:id', userInfos)
+                    .then (function (response) {
+                        resolve(response);
+                    })
+                    .catch (function (erreur) {
+                        reject(erreur);
+                    });
+            })
+        },
+        createCount : ({commit}, userInfos) => {
+            return new Promise((resolve, reject) => {
+                commit;
+                instance.post('/user', userInfos)
+                    .then (function (response) {
+                        resolve(response);
+                    })
+                    .catch (function (erreur) {
+                        reject(erreur);
+                    });
+            })
+        }
+    }
+})
 
 export default store;
