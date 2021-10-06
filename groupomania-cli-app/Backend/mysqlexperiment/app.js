e3a910a66d7d5313ccbbf2270622a84b584e591f
David <davidroussel171113@outlook.fr>
2021-10-06 22:35:35 +0200

    Utilisation de bodyparser

--- groupomania-cli-app/Backend/mysqlexperiment/app.js
+++ groupomania-cli-app/Backend/mysqlexperiment/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const mysql = require('./Security/mysql');
+const bodyParser = require('body-parser');
 const moduleSecurity = require('./Security/module');
 const app = express();
+
+//Header utiliser par l'application.
+app.use(((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+}));
+
+app.use(bodyParser.json());
 
 const postRoutes = require('./routes/post');
 const userRoutes = require('./routes/user');
