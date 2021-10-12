81d5c2ebb8a053b631ce82824a051877ef7fc856
David <davidroussel171113@outlook.fr>
2021-10-13 00:41:41 +0200

    Modification action du store

--- groupomania-cli-app/Frontend/src/store/index.js
+++ groupomania-cli-app/Frontend/src/store/index.js
@@ -3,42 +3,34 @@ import { createStore } from "vuex"
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL : 'http://localhost:3000/groupomania'
+    baseURL : 'http://localhost:3000/user',
 });
-
-const instanceLog = axios.create({
-    baseURL : 'http://localhost:3000/groupomania/user'
-})
 
 const store = createStore({
     state : {
-
     },
     actions : {
         login : ({commit}, userInfos) => {
-            return new Promise((resolve, reject) => {
-                commit;
-                instanceLog.get('/:id', userInfos)
-                    .then (function (response) {
-                        resolve(response);
-                    })
-                    .catch (function (erreur) {
-                        reject(erreur);
-                    });
-            })
+            commit;
+            instance.get('/:id', userInfos)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (erreur) {
+                    console.log(erreur);
+                });
         },
-        createCount : ({commit}, userInfos) => {
-            return new Promise((resolve, reject) => {
+        createAccount : ({commit}, userInfos) => {
                 commit;
-                instance.post('/user', userInfos)
+                instance.post('/create', userInfos)
                     .then (function (response) {
-                        resolve(response);
+                        console.log(response)
                     })
                     .catch (function (erreur) {
-                        reject(erreur);
+                        console.log(erreur)
                     });
-            })
-        }
+        },
+
     }
 })
 
