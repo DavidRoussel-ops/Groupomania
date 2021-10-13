bf5b7f96bf9b33339572e7d1cb856ad6539abc08
David <davidroussel171113@outlook.fr>
2021-10-13 13:06:47 +0200

    Refonte du controllers utilisateur

--- groupomania-cli-app/Backend/mysqlexperiment/controllers/user.js
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/user.js
@@ -1,11 +1,10 @@
 const db = require('../Security/mysql');
-const {getUser, getUserById, insertUser, updateUserById, deleteUserById} = require("../models/user");
 
 exports.showUser = (req, res) => {
     db.query('SELECT * FROM utilisateur', (err , results) => {
         if (err) {
             console.log(err);
-            res.status(400).json({message : 'Oups !'});
+            res.status(400).json({message : 'Liste des utilisateurs non trouvé !'});
         } else {
             console.log({users : results});
             res.status(200).json({message : results});
@@ -15,12 +14,12 @@ exports.showUser = (req, res) => {
 
 exports.showUserById = (req, res) => {
     let id = req.params.id;
-    db.query('SELECT * FROM utilisateur WHERE id =?', [id], (err, results) => {
+    db.query('SELECT * FROM utilisateur WHERE id =' + id, (err, results) => {
         if (err) {
             console.log(err);
-            res.status(400).json({message : 'Oups !'});
+            res.status(400).json({message : 'Utilisateur non trouvé !'});
         } else {
-            console.log({users : results});
+            console.log({userId : results});
             res.status(200).json({message : results});
         }
     });
@@ -31,11 +30,11 @@ exports.createUser = (req, res) => {
     let mdp = req.body.pass;
     let lastname = req.body.lname;
     let firstname = req.body.fname;
-    let insertUser = "INSERT INTO utilisateur (mail, pass, lname, fname) VALUES ('"+email+"','"+mdp+"','"+lastname+"','"+firstname+"')";
+    let insertUser = "INSERT INTO utilisateur (mail, pass, lname, fname)VALUES ('"+email+"','"+mdp+"','"+lastname+"','"+firstname+"')";
     db.query(insertUser, (err, results) => {
         if (err) {
             console.log(err);
-            res.status(400).json({message : "Oups!"})
+            res.status(400).json({message : "Utilisateur non crée!"})
         } else {
             console.log({userInsert : results});
             res.status(201).json({message : "utilisateur crée!!"})
@@ -44,28 +43,35 @@ exports.createUser = (req, res) => {
 };
 
 exports.updateUser = (req, res) => {
-    const data = req.body;
-    const id = req.params.id;
-    updateUserById(data, id,(err, results) => {
-        if (err) {
-            res.send(err);
-            console.log(err)
-        } else {
-            res.json(results);
-            console.log({userUpdate : results})
-        }
-    });
+    db.connect(function () {
+        let mdp = req.params.pass;
+        let lastname = req.params.lname;
+        let firstname = req.params.fname;
+        let id = req.params.id;
+        let newmdp = req.body.pass;
+        let newlastname = req.body.lname;
+        let newfirstname = req.body.fname;
+        db.query('UPDATE utilisateur SET pass = ' + mdp + ', lname = ' + lastname + ',fname = ' + firstname + ', id= ' + id + ' WHERE pass=' + newmdp + ', lname =' + newlastname + ', fname = ' + newfirstname + '', (err, results) => {
+            if (err) {
+                console.log(err);
+                res.status(400).json({message: "Utilisateur non modifié!"});
+            } else {
+                console.log({userUpdate: results});
+                res.status(200).json({message: results})
+            }
+        });
+    })
 };
 
 exports.deleteUser = (req, res) => {
-    const id = req.params.id;
-    deleteUserById(id, (err, results) => {
+    let id = req.params.id;
+    db.query('DELETE FROM utilisateur WHERE id =' + id, (err, results) => {
         if (err) {
-            res.send(err);
-            console.log(err)
+            console.log(err);
+            res.status(400).json({message: "Utilisateur non supprimé!"});
         } else {
-            res.json(results);
-            console.log({userdelete : results})
+            console.log({userDelete: results});
+            res.status(200).json({message: results})
         }
     });
 };
