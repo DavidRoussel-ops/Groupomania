7b9dbce8624fa7fd78692349c93f3032eb8a8ec1
David <davidroussel171113@outlook.fr>
2021-10-13 13:08:55 +0200

    Modification route utilisateur

--- groupomania-cli-app/Backend/mysqlexperiment/routes/user.js
+++ groupomania-cli-app/Backend/mysqlexperiment/routes/user.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const {showUser, showUserById, createUser, updateUser, deleteUser} = require("../controllers/user");
 
-router.get('/user', showUser);
+router.get('/all', showUser);
 router.get('/:id', showUserById);
 router.post('/create', createUser);
-router.put('/user/:id', updateUser);
-router.delete('/user/:id', deleteUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
