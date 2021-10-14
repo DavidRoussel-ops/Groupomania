1aebd24fed19be0587cafa5bf98e49641762966a
David <davidroussel171113@outlook.fr>
2021-10-14 16:12:03 +0200

    Refonte commentaire et ajout jwt

--- groupomania-cli-app/Backend/mysqlexperiment/controllers/com.js
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/com.js
@@ -1,65 +1,49 @@
-const {getCom, getComById, insertCom, updateComById, deleteComByID} = require("../models/com");
+const db = require('../mysql');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const jwtSecurity = require('../Security/tokenSecurity');
 
-exports.showCom = (req, res) => {
-    getCom((err, results) => {
-        if (err) {
-            res.send(err);
-            console.log(err)
-        } else {
-            res.json(results);
-            console.log({com : results})
-        }
-    });
-};
-
-exports.showComById = (req, res) => {
-    getComById(req.params.id, (err, results) => {
-        if (err) {
-            res.send(err);
-            console.log(err)
-        } else {
-            res.json(results);
-            console.log({comId : results})
-        }
-    });
-};
 
 exports.createCom = (req, res) => {
-    const data = req.body;
-    insertCom(data, (err, results) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    const post_id = req.params.id;
+    const content = req.body.content;
+    db.query(`INSERT INTO com VALUES (NULL, ?, ?, NOW(), ?)`, [userId, post_id, content], function (err, result) {
         if (err) {
-            res.send(err);
-            console.log(err)
+            console.log(err);
+            return res.status(400).json("error");
         } else {
-            res.json(results);
-            console.log({comCreate : results})
+            console.log("1 nouveau commentaire");
+            res.status(201).json({ message: "1 nouveau commentaire" });
         }
     });
 };
 
-exports.updateCom = (req, res) => {
-    const data = req.body;
-    const id = req.params.id;
-    updateComById(data, id,(err, results) => {
+exports.showCom = (req, res) => {
+    const post_id = req.params.id;
+    db.query(`SELECT utilisateur.fname, utilisateur.admin, com.id, com.content, com.date, com.user_id FROM utilisateur INNER JOIN com ON utilisateur.id = com.user_id WHERE com.post_id = ? ORDER BY id desc`, [post_id], function (err, result) {
         if (err) {
-            res.send(err);
-            console.log(err)
+            console.log(err);
+            return res.status(400).json("error");
         } else {
-            res.json(results);
-            console.log({comUpdate : results})
+            console.log("Tous les commentaires.");
+            res.status(201).json(result);
         }
     });
 };
 
 exports.deleteCom = (req, res) => {
-    const id = req.params.id;
-    deleteComByID(id, (err, results) => {
+    const com_id = req.body.id;
+    let sql = `DELETE FROM com WHERE id=${com_id}`;
+    db.query(sql, function (err, result) {
         if (err) {
-            res.send(err);
-            console.log(err)
+            console.log(err);
+            return res.status(400).json("error");
         } else {
-            res.json(results);
-            console.log({comdelete : results})
+            console.log("Commentaire supprimé.");
+            return res.status(200).json(result);
         }
     });
 };
