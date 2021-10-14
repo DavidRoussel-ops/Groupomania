c42f9bc4bf4d30f7306f90353313a6731a4e2a01
David <davidroussel171113@outlook.fr>
2021-10-14 16:11:22 +0200

    Refonte controller et ajout jwt

--- groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
@@ -1,65 +1,64 @@
-const {getPost, getPostById, insertPost, updatePostById, deletePostById} = require("../models/post");
+const db = require('../mysql');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const jwtSecurity = require('../Security/tokenSecurity');
+
+
+exports.createPost = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    const com = req.body.com;
+    let img = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+    db.query(`INSERT INTO post VALUES (NULL, ?, NOW(), ?, ?)`, [userId, com, img], function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.status(400).json("error");
+        } else {
+            console.log("1 post enregistrer");
+            res.status(201).json({ message: "1 post enregistre" });
+        }
+    });
+};
 
 exports.showPost = (req, res) => {
-    getPost((err, results) => {
+    db.query(`SELECT utilisateur.fname, post.id, post.com, post.img, post.date, post.user_id FROM utilisateur INNER JOIN post ON utilisateur.id = post.user_id ORDER BY date desc`, function (err, result) {
         if (err) {
-            res.send(err);
-            console.log(err)
+            console.log(err);
+            return res.status(400).json("error");
         } else {
-            res.json(results);
-            console.log({Post : results})
+            res.status(201).json(result);
         }
     });
 };
 
 exports.showPostById = (req, res) => {
-    getPostById(req.params.id, (err, results) => {
+    let post_id = req.params.id;
+    db.query(`SELECT utilisateur.id, utilisateur.mail, utilisateur.lname, utilisateur.fname, utilisateur.admin, post.id, post.com, post.img, post.date, post.user_id FROM utilisateur INNER JOIN post ON utilisateur.id = post.user_id WHERE post.id = ?`, [post_id], function (err, result) {
         if (err) {
-            res.send(err);
-            console.log(err)
+            console.log(err);
+            return res.status(400).json("error");
         } else {
-            res.json(results);
-            console.log({PostId : results})
-        }
-    });
-};
-
-exports.createPost = (req, res) => {
-    const data = req.body;
-    insertPost(data, (err, results) => {
-        if (err) {
-            res.send(err);
-            console.log(err)
-        } else {
-            res.json(results);
-            console.log({postCreate : results})
-        }
-    });
-};
-
-exports.updatePost = (req, res) => {
-    const data = req.body;
-    const id = req.params.id;
-    updatePostById(data, id,(err, results) => {
-        if (err) {
-            res.send(err);
-            console.log(err)
-        } else {
-            res.json(results);
-            console.log({postUpdate : results})
+            res.status(201).json(result);
         }
     });
 };
 
 exports.deletePost = (req, res) => {
-    const id = req.params.id;
-    deletePostById(id, (err, results) => {
-        if (err) {
-            res.send(err);
-            console.log(err)
-        } else {
-            res.json(results);
-            console.log({postdelete : results})
-        }
-    });
+    const post_id = req.params.id;
+    const filename = req.body.img;
+    const goodFile = filename.split('/images/')[1];
+    let sql = `DELETE FROM post WHERE id=${post_id}`;
+    fs.unlink(`images/${goodFile}`, () => {
+        db.query(sql, function (err, result) {
+            if (err) {
+                console.log(err);
+                return res.status(400).json("error");
+            } else {
+
+                console.log("Post supprimé");
+                return res.status(200).json(result);
+            }
+        });
+    })
 };
