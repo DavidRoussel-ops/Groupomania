e91ff18c47621d00926f6fa4ae49ea8a15506115
David <davidroussel171113@outlook.fr>
2021-10-14 18:53:08 +0200

    ajout route groupomania

--- groupomania-cli-app/Frontend/src/router/index.js
+++ groupomania-cli-app/Frontend/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter , createWebHashHistory } from 'vue-router'
 import Home from '../views/Home';
+import groupomania from "@/views/groupomania";
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/groupomania',
+    name: 'Groupomania',
+    component: groupomania
   },
 ]
 
