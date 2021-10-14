6885aeb0d887795e44154cd04d659a581575c310
David <davidroussel171113@outlook.fr>
2021-10-14 18:54:38 +0200

    Ajout de state, mutation et action en cas de log

--- groupomania-cli-app/Frontend/src/store/index.js
+++ groupomania-cli-app/Frontend/src/store/index.js
@@ -8,27 +8,49 @@ const instance = axios.create({
 
 const store = createStore({
     state : {
+        status : '',
+        user : {
+            result : [],
+            token : '',
+        },
+    },
+    mutations : {
+        setStatus : function (state, status) {
+            state.status = status;
+        },
+        logUser : function (state, user) {
+            state.user = user;
+        }
     },
     actions : {
         login : ({commit}, userInfos) => {
-            commit;
-            instance.get('/:id', userInfos)
+            commit('setStatus', 'loading');
+            return new Promise((resolve, reject) => {
+            instance.post('/login', userInfos)
                 .then(function (response) {
-                    console.log(response);
+                    commit('setStatus', '');
+                    commit('logUser', response.data);
+                    resolve(response);
                 })
                 .catch(function (erreur) {
-                    console.log(erreur);
+                    commit('setStatus', 'error_login');
+                    reject(erreur);
                 });
+        });
         },
         createAccount : ({commit}, userInfos) => {
+            return new Promise((resolve, reject) => {
                 commit;
                 instance.post('/create', userInfos)
-                    .then (function (response) {
-                        console.log(response)
+                    .then(function (response) {
+                        commit('setStatus', 'created');
+                        resolve(response);
                     })
-                    .catch (function (erreur) {
-                        console.log(erreur)
+                    .catch(function (erreur) {
+                        commit('setStatus', 'error_create');
+                        reject(erreur);
                     });
+            });
         },
 
     }
