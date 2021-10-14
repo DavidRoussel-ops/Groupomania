46ca844e93df259cd56f01299c42503d364fee05
David <davidroussel171113@outlook.fr>
2021-10-14 16:13:35 +0200

    Suppression route update

--- groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {showPost, showPostById, createPost, updatePost, deletePost} = require('../controllers/post');
+const {showPost, showPostById, createPost, deletePost} = require('../controllers/post');
 
-router.get('/post', showPost);
-router.get('/post/:id', showPostById);
-router.post('/post', createPost);
-router.put('/post/:id', updatePost);
-router.delete('/post/:id', deletePost);
+router.get('/all', showPost);
+router.get('/:id', showPostById);
+router.post('/create', createPost);
+router.delete('/delete', deletePost);
 
 module.exports = router;
