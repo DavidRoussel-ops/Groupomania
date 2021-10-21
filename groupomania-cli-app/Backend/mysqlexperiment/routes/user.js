e8e8fe61f1aee8c3df74b47c7d43029c162f5730
David <davidroussel171113@outlook.fr>
2021-10-21 13:47:35 +0200

    modification route utilisateur

--- groupomania-cli-app/Backend/mysqlexperiment/routes/user.js
+++ groupomania-cli-app/Backend/mysqlexperiment/routes/user.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userToken = require('../Security/token');
-const {signup, login, showUser, showUserById, updateUser, deleteUser} = require("../controllers/user");
+const multer = require('../middleware/multer');
+const userCtrl = require("../controllers/user");
 
-router.get('/all', showUser);
-router.get('/info', userToken, showUserById);
-router.post('/create', signup);
-router.post('/login', login);
-router.put('/update', userToken, updateUser);
-router.delete('/delete',userToken, deleteUser);
+try {
+    router.get('/id', userToken, userCtrl.showUserById);
+    router.post('/create', userCtrl.signup);
+    router.post('/login', userCtrl.login);
+    router.put('/update', userToken, userCtrl.updateUser);
+    router.delete('/delete', userCtrl.deleteUser);
+} catch (error) {
+    console.log(error);
+}
 
 module.exports = router;
