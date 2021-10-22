bfb9afb277a2f18c84f1b9d148c64afadb4d52dc
David <davidroussel171113@outlook.fr>
2021-10-22 15:13:16 +0200

    Modification route get put delete

--- groupomania-cli-app/Backend/mysqlexperiment/routes/user.js
+++ groupomania-cli-app/Backend/mysqlexperiment/routes/user.js
@@ -5,11 +5,11 @@ const multer = require('../middleware/multer');
 const userCtrl = require("../controllers/user");
 
 try {
-    router.get('/id', userToken, userCtrl.showUserById);
     router.post('/create', userCtrl.signup);
     router.post('/login', userCtrl.login);
-    router.put('/update', userToken, userCtrl.updateUser);
-    router.delete('/delete', userCtrl.deleteUser);
+    router.get('/', userToken, userCtrl.showUserById);
+    router.put('/', userToken, userCtrl.updateUser);
+    router.delete('/', userToken, userCtrl.deleteUser);
 } catch (error) {
     console.log(error);
 }
