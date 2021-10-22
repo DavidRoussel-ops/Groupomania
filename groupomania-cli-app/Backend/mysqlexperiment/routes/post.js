61b2f8c6050f814f9246bf6044cc07d67f3a97ed
David <davidroussel171113@outlook.fr>
2021-10-22 15:12:38 +0200

    Mise en place route post et commentaire

--- groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const {showPost, showPostById, createPost, deletePost} = require('../controllers/post');
+const userToken = require('../Security/token');
+const multer = require('../middleware/multer');
+const postCtrl = require('../controllers/post');
 
-router.get('/all', showPost);
-router.get('/:id', showPostById);
-router.post('/create', createPost);
-router.delete('/delete', deletePost);
+try {
+    router.get('/', userToken, postCtrl.allPost);
+    router.post('/', userToken, postCtrl.createPost);
+    router.put('/:id', userToken, postCtrl.updatePost);
+    router.delete('/:id', userToken, postCtrl.deletePost);
+
+    router.get('/:id/comment', userToken, postCtrl.comment);
+    router.post('/:id/comment', userToken, postCtrl.createCom);
+    router.put('/comment/:id', userToken, postCtrl.updateCom);
+    router.delete('/comment/:id', userToken, postCtrl.deleteCom);
+} catch (error) {
+    console.log(error);
+}
 
 module.exports = router;
