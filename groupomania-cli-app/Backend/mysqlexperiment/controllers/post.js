50b10ec945f89c58f31efc78b98ecd950fd763d0
David <davidroussel171113@outlook.fr>
2021-10-22 15:11:06 +0200

    Refonte pour postman

--- groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
@@ -1,64 +1,121 @@
 const db = require('../mysql');
+const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
-const fs = require('fs');
 const jwtSecurity = require('../Security/tokenSecurity');
+const PostModels = require('../models/postModels');
 
+let postModels = new PostModels();
+
+exports.allPost = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    let sqlInserts = [userId];
+    postModels.allPost(sqlInserts)
+        .then((response) => {
+            res.status(200).json(JSON.stringify(response));
+        });
+};
 
 exports.createPost = (req, res) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, jwtSecurity);
     const userId = decodedToken.userId;
-    const com = req.body.com;
-    let img = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
-    db.query(`INSERT INTO post VALUES (NULL, ?, NOW(), ?, ?)`, [userId, com, img], function (err, result) {
-        if (err) {
-            console.log(err);
-            return res.status(400).json("error");
-        } else {
-            console.log("1 post enregistrer");
-            res.status(201).json({ message: "1 post enregistre" });
-        }
-    });
+    let com = req.body.com;
+    let img = req.body.img;
+    let sqlInserts = [userId, com, img];
+    postModels.createPost(sqlInserts)
+        .then((response) => {
+            res.status(201).json(JSON.stringify(response));
+        })
 };
 
-exports.showPost = (req, res) => {
-    db.query(`SELECT utilisateur.fname, post.id, post.com, post.img, post.date, post.user_id FROM utilisateur INNER JOIN post ON utilisateur.id = post.user_id ORDER BY date desc`, function (err, result) {
-        if (err) {
-            console.log(err);
-            return res.status(400).json("error");
-        } else {
-            res.status(201).json(result);
-        }
-    });
-};
-
-exports.showPostById = (req, res) => {
-    let post_id = req.params.id;
-    db.query(`SELECT utilisateur.id, utilisateur.mail, utilisateur.lname, utilisateur.fname, utilisateur.admin, post.id, post.com, post.img, post.date, post.user_id FROM utilisateur INNER JOIN post ON utilisateur.id = post.user_id WHERE post.id = ?`, [post_id], function (err, result) {
-        if (err) {
-            console.log(err);
-            return res.status(400).json("error");
-        } else {
-            res.status(201).json(result);
-        }
-    });
+exports.updatePost = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    let com = req.body.com;
+    let img = req.body.img;
+    let postId = req.params.id;
+    let sqlInserts1 = [postId];
+    let sqlInserts2 = [com, img, postId, userId];
+    postModels.modifyPost(sqlInserts1, sqlInserts2)
+        .then((response) => {
+            res.status(201).json(JSON.stringify(response));
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(400).json(JSON.stringify(error));
+        })
 };
 
 exports.deletePost = (req, res) => {
-    const post_id = req.params.id;
-    const filename = req.body.img;
-    const goodFile = filename.split('/images/')[1];
-    let sql = `DELETE FROM post WHERE id=${post_id}`;
-    fs.unlink(`images/${goodFile}`, () => {
-        db.query(sql, function (err, result) {
-            if (err) {
-                console.log(err);
-                return res.status(400).json("error");
-            } else {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    const postId = req.params.id;
+    let sqlInserts1 = [postId];
+    let sqlInserts2 = [postId, userId];
+    postModels.delPost(sqlInserts1, sqlInserts2)
+        .then((response) => {
+            res.status(200).json(JSON.stringify(response));
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(400).json(JSON.stringify(error));
+        })
+};
 
-                console.log("Post supprimé");
-                return res.status(200).json(result);
-            }
+exports.comment = (req, res) => {
+    postModels.comment()
+        .then((response) => {
+            res.status(200).json(JSON.stringify(response));
         });
-    })
+};
+
+exports.createCom = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    let com = req.body.com;
+    let sqlInserts = [com, userId];
+    postModels.createCom(sqlInserts)
+        .then((response) => {
+            res.status(201).json(JSON.stringify(response))
+        })
+};
+
+exports.updateCom = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    let com = req.body.com;
+    let comId = req.params.id;
+    let sqlInserts1 = [comId];
+    let sqlInserts2 = [com, comId, userId];
+    postModels.modifyCom(sqlInserts1, sqlInserts2)
+        .then((response) => {
+            res.status(201).json(JSON.stringify(response));
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(400).json(JSON.stringify(error));
+        })
+};
+
+exports.deleteCom = (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, jwtSecurity);
+    const userId = decodedToken.userId;
+    const comId = req.params.id;
+    let sqlInserts1 = [comId];
+    let sqlInserts2 = [comId, userId];
+    postModels.delCom(sqlInserts1, sqlInserts2)
+        .then((response) => {
+            res.status(200).json(JSON.stringify(response));
+        })
+        .catch((error) => {
+            console.log(error);
+            res.status(400).json(JSON.stringify(error));
+        })
 };
--- groupomania-cli-app/Backend/mysqlexperiment/routes/com.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {showCom, createCom, deleteCom} = require('../controllers/com')
-
-router.get('/all', showCom);
-router.post('/create', createCom);
-router.delete('/delete', deleteCom);
-
-module.exports = router;
