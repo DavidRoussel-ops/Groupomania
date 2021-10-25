a58208042cbdfbf72d44ddc7775676f3dec37324
David <davidroussel171113@outlook.fr>
2021-10-25 22:48:57 +0200

    Changement route pour les commentaires

--- groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/routes/post.js
@@ -5,15 +5,15 @@ const multer = require('../middleware/multer');
 const postCtrl = require('../controllers/post');
 
 try {
-    router.get('/', userToken, postCtrl.allPost);
-    router.post('/', userToken, postCtrl.createPost);
+    router.get('/', postCtrl.allPost);
+    router.post('/', postCtrl.createPost);
     router.put('/:id', userToken, postCtrl.updatePost);
     router.delete('/:id', userToken, postCtrl.deletePost);
 
-    router.get('/:id/comment', userToken, postCtrl.comment);
-    router.post('/:id/comment', userToken, postCtrl.createCom);
-    router.put('/comment/:id', userToken, postCtrl.updateCom);
-    router.delete('/comment/:id', userToken, postCtrl.deleteCom);
+    router.get('/:id/comments', postCtrl.comment);
+    router.post('/:id/comments', postCtrl.createCom);
+    router.put('/comments/:id', userToken, postCtrl.updateCom);
+    router.delete('/comments/:id', userToken, postCtrl.deleteCom);
 } catch (error) {
     console.log(error);
 }
