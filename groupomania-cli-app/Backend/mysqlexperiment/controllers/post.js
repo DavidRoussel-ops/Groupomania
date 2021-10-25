8ac1b4a67b53f109db0e68f646cb2e55a724446a
David <davidroussel171113@outlook.fr>
2021-10-25 22:45:01 +0200

    Modification du au changement de BDD

--- groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
+++ groupomania-cli-app/Backend/mysqlexperiment/controllers/post.js
@@ -3,24 +3,19 @@ const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
 const jwtSecurity = require('../Security/tokenSecurity');
 const PostModels = require('../models/postModels');
+require('dotenv').config();
 
 let postModels = new PostModels();
 
 exports.allPost = (req, res) => {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, jwtSecurity);
-    const userId = decodedToken.userId;
-    let sqlInserts = [userId];
-    postModels.allPost(sqlInserts)
+    postModels.allPost()
         .then((response) => {
             res.status(200).json(JSON.stringify(response));
         });
 };
 
 exports.createPost = (req, res) => {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, jwtSecurity);
-    const userId = decodedToken.userId;
+    let userId = req.body.userId;
     let com = req.body.com;
     let img = req.body.img;
     let sqlInserts = [userId, com, img];
@@ -67,18 +62,19 @@ exports.deletePost = (req, res) => {
 };
 
 exports.comment = (req, res) => {
-    postModels.comment()
+    let postId = req.params.id;
+    let sqlInserts = [postId];
+    postModels.comment(sqlInserts)
         .then((response) => {
             res.status(200).json(JSON.stringify(response));
         });
 };
 
 exports.createCom = (req, res) => {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, jwtSecurity);
-    const userId = decodedToken.userId;
-    let com = req.body.com;
-    let sqlInserts = [com, userId];
+    let userId = req.body.userId;
+    const postId = req.params.id;
+    let com = req.body.content;
+    let sqlInserts = [userId, postId, com];
     postModels.createCom(sqlInserts)
         .then((response) => {
             res.status(201).json(JSON.stringify(response))
@@ -89,7 +85,7 @@ exports.updateCom = (req, res) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, jwtSecurity);
     const userId = decodedToken.userId;
-    let com = req.body.com;
+    let com = req.body.content;
     let comId = req.params.id;
     let sqlInserts1 = [comId];
     let sqlInserts2 = [com, comId, userId];
